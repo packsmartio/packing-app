c5dd070630ac0c959cab9d014dae056dcaabb442
packsmartio <ytakayanagi19910203@gmail.com>
2026-05-20 01:26:29 +0900

    Update sw.js

--- public/sw.js
+++ public/sw.js
@@ -1,16 +1,13 @@
-const CACHE_NAME = 'pack-smart-v1';
+const CACHE_NAME = 'pack-smart-v2';
 const ASSETS = ['/', '/index.html', '/icon-192.png', '/icon-180.png', '/icon-512.png', '/manifest.json'];
-
 self.addEventListener('install', e => {
   e.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS)));
   self.skipWaiting();
 });
-
 self.addEventListener('activate', e => {
   e.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k)))));
   self.clients.claim();
 });
-
 self.addEventListener('fetch', e => {
-  e.respondWith(caches.match(e.request).then(cached => cached || fetch(e.request).catch(() => caches.match('/index.html'))));
+  e.respondWith(fetch(e.request).catch(() => caches.match(e.request).then(cached => cached || caches.match('/index.html'))));
 });
